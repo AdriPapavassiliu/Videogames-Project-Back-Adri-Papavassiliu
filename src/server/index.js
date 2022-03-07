05dc8e9bd83efe8f3e2b66a012317529a7d255bb
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-07 17:06:13 +0100

    added notFoundError and generalError

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const { default: helmet } = require("helmet");
+const { notFoundError, generalError } = require("../middlewares/errors");
 
 const app = express();
 
@@ -9,5 +10,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
