a6be09078388b23503b1626603b56c855b61c4c6
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-08 10:48:38 +0100

    added Videogame model

---
+++ src/database/models/Videogame.js
@@ -0,0 +1,31 @@
+const { model, Schema } = require("mongoose");
+
+const VideogameSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  platforms: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+  },
+});
+
+const Videogame = model("Videogame", VideogameSchema, "videogames");
+
+module.exports = Videogame;
