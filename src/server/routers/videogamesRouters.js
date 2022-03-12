64480158f1bba2de1720953e632eb56f5c148f65
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-12 13:04:04 +0100

    added deleteVideogame router and tested endpoint

--- src/server/routers/videogamesRouters.js
+++ src/server/routers/videogamesRouters.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const { getAllVideogames } = require("../controllers/videogamesControllers");
+const {
+  getAllVideogames,
+  deleteVideogame,
+} = require("../controllers/videogamesControllers");
 
 const router = express.Router();
 
 router.get("/", getAllVideogames);
+router.delete("/:videogameId", deleteVideogame);
 
 module.exports = router;
