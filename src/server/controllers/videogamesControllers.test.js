c226df863700247d47b0fa65c9d73e2430f0ffc0
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-15 11:01:00 +0100

    tested createVideogame controller

--- src/server/controllers/videogamesControllers.test.js
+++ src/server/controllers/videogamesControllers.test.js
@@ -1,10 +1,19 @@
+const fs = require("fs");
 const Videogame = require("../../database/models/Videogame");
 const {
   getAllVideogames,
   deleteVideogame,
+  createVideogame,
 } = require("./videogamesControllers");
 
 jest.mock("../../database/models/Videogame");
+
+jest.mock("firebase/storage", () => ({
+  getStorage: () => "holaa",
+  ref: () => {},
+  getDownloadURL: async () => "download.url",
+  uploadBytes: async () => {},
+}));
 
 describe("Given a getAllVideogames controller", () => {
   beforeEach(() => {
@@ -74,6 +83,160 @@ describe("Given a deleteVideogame controller", () => {
       await deleteVideogame(req, null, next);
 
       expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
+
+describe("Given a createVideogame controller", () => {
+  describe("When it's instantiated with a new videogame in the body and an image as file", () => {
+    test("Then it should call json with the new videogame and the firebase url as image property", async () => {
+      const newVideogame = {
+        name: "Hola",
+        platforms: ["PS4, XBOX, PS5, PC"],
+        genre: "Shooter",
+        description: "Hola",
+        year: 2019,
+      };
+      const newFile = {
+        fieldname: "image",
+        originalname: "hola.jpeg",
+        encoding: "7bit",
+        mimetype: "image/jpeg",
+        destination: "uploads/",
+        filename: "93ec034d18753a982e662bc2fdf9a584",
+        path: "uploads/93ec034d18753a982e662bc2fdf9a584",
+        size: 8750,
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      jest
+        .spyOn(fs, "rename")
+        .mockImplementation((oldpath, newpath, callback) => {
+          callback();
+        });
+
+      const req = {
+        body: newVideogame,
+        file: newFile,
+      };
+      const next = jest.fn();
+      Videogame.create = jest.fn().mockResolvedValue("Hola");
+
+      jest.spyOn(fs, "readFile").mockImplementation((file, callback) => {
+        callback(null, newFile);
+      });
+
+      await createVideogame(req, res, next);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it's instantiated with a new videogame in the body and an image in the file, and has an error on fs.rename", () => {
+    test("Then it should should call next with an error", async () => {
+      const newVideogame = {
+        name: "Hola",
+        platforms: ["PS4, XBOX, PS5, PC"],
+        genre: "Shooter",
+        description: "Hola",
+        year: 2019,
+      };
+      const newFile = {
+        fieldname: "image",
+        originalname: "hola.jpeg",
+        encoding: "7bit",
+        mimetype: "image/jpeg",
+        destination: "uploads/",
+        filename: "93ec034d18753a982e662bc2fdf9a584",
+        path: "uploads/93ec034d18753a982e662bc2fdf9a584",
+        size: 8750,
+      };
+
+      const req = {
+        body: newVideogame,
+        file: newFile,
+      };
+      const next = jest.fn();
+
+      jest.spyOn(fs, "readFile").mockImplementation((file, callback) => {
+        callback("error", null);
+      });
+
+      await createVideogame(req, null, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives a request with file and no videogame on body", () => {
+    test("Then it should call next with an error", async () => {
+      const newFile = {
+        fieldname: "image",
+        originalname: "hola.jpeg",
+        encoding: "7bit",
+        mimetype: "image/jpeg",
+        destination: "uploads/",
+        filename: "93ec034d18753a982e662bc2fdf9a584",
+        path: "uploads/93ec034d18753a982e662bc2fdf9a584",
+        size: 8750,
+      };
+
+      const req = {
+        file: newFile,
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+
+      jest.spyOn(fs, "unlink").mockImplementation((path, callback) => {
+        callback();
+      });
+
+      await createVideogame(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it has an error when renaming the file", () => {
+    test("Then it should call the next method with an error", async () => {
+      const newVideogame = {
+        name: "Hola",
+        platforms: ["PS4, XBOX, PS5, PC"],
+        genre: "Shooter",
+        description: "Hola",
+        year: 2019,
+      };
+      const newFile = {
+        fieldname: "image",
+        originalname: "hola.jpeg",
+        encoding: "7bit",
+        mimetype: "image/jpeg",
+        destination: "uploads/",
+        filename: "93ec034d18753a982e662bc2fdf9a584",
+        path: "uploads/93ec034d18753a982e662bc2fdf9a584",
+        size: 8750,
+      };
+
+      const req = {
+        body: newVideogame,
+        file: newFile,
+      };
+      const next = jest.fn();
+
+      jest
+        .spyOn(fs, "rename")
+        .mockImplementation((oldpath, newpath, callback) => {
+          callback("error");
+        });
+
+      await createVideogame(req, null, next);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
