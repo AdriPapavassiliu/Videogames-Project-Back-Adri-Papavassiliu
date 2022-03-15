f0019f8721fffd6a4bdbfb062be02418e048ec6c
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-15 13:10:53 +0100

    modified joi validation

--- src/server/routers/videogamesRouters.js
+++ src/server/routers/videogamesRouters.js
@@ -1,18 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const { validate } = require("express-validation");
 const multer = require("multer");
 const {
   getAllVideogames,
   deleteVideogame,
   createVideogame,
 } = require("../controllers/videogamesControllers");
-const joiValidator = require("../middlewares/joiValidator");
+const videogamaeJoi = require("../schemas/videogameJoi");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
 router.get("/", getAllVideogames);
 router.delete("/:videogameId", deleteVideogame);
-router.post("/create", upload.single("image"), joiValidator, createVideogame);
+router.post(
+  "/create",
+  validate(videogamaeJoi),
+  upload.single("image"),
+  createVideogame
+);
 
 module.exports = router;
